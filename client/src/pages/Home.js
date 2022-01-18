a3d1e2fca55a3e6d93a6655d4b3b90ebc19d7b50
Nashid-Alam <nashid17@gmail.com>
2022-01-18 10:23:04 -0500

    added axios call to get neighbhood and added post

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+
+
+import React from 'react'
+import SearchBar from '../components/SearchBar'
+
+function Home(props) {
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  )
+}
+
+export default Home
+
--- controllers/CrimeTipController.js
+++ controllers/CrimeTipController.js
@@ -1,4 +1,4 @@
-const { CrimeTip, Comment } = require('../models');
+const { CrimeTip, Comment, Neighborhood } = require('../models');
 const CreateCrimeTip = async (req, res) => {
   console.log(req.body);
   try {
@@ -50,10 +50,22 @@ const deleteCrimeTip = async (req, res) => {
     throw error;
   }
 };
+
+const searchCrimeTip = async (req, res) => {
+  try{
+    // const {name} = req.query;
+    let item = await CrimeTip.findAll({include: [{model: Neighborhood, where: {'zipcode' : req.query.zipcode}}]})
+    return res.status(200).json({ item });
+  }catch (error){
+    return res.status(500).send(error.message);
+  }
+}
+
 module.exports = {
   CreateCrimeTip,
   deleteCrimeTip,
   updateCrimeTip,
   getAllCrimeTips,
-  getCrimeTipById
+  getCrimeTipById,
+  searchCrimeTip
 };

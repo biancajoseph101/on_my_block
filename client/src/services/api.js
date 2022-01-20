46c30c30a4a5a0458edf5aa595dfb72376170ccb
biancajoseph101 <biancajoseph101@gmail.com>
2022-01-20 16:50:28 -0600

    changed baseurl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
+import { BaseURL } from '../globals';
 
-export const BASE_URL = 'http://localhost:3001';
-const Client = Axios.create({ baseURL: BASE_URL });
+const Client = Axios.create({ baseURL: BaseURL });
 // Intercepts every request axios makes
 Client.interceptors.request.use(
   (config) => {
